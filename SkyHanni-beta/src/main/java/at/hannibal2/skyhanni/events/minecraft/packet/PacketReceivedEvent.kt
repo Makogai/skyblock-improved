bbf0551578749a93c8b955b1c9367d43ca4338d2
Marko <mare.sampbn@gmail.com>
2026-02-08 15:36:47 +0100

    initial

--- SkyHanni-beta/src/main/java/at/hannibal2/skyhanni/events/minecraft/packet/PacketReceivedEvent.kt
+++
@@ -1,6 +0,0 @@
-package at.hannibal2.skyhanni.events.minecraft.packet
-
-import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-import net.minecraft.network.protocol.Packet
-
-class PacketReceivedEvent(val packet: Packet<*>) : SkyHanniEvent()
--- SkyHanni-beta/src/main/java/at/hannibal2/skyhanni/utils/ParticleUtils.kt
+++
@@ -1,9 +0,0 @@
-package at.hannibal2.skyhanni.utils
-
-import net.minecraft.resources.Identifier
-
-object ParticleUtils {
-
-    fun getParticleTypeByName(name: String, shouldError: Boolean = false): Identifier? = Identifier.parse(name.lowercase())
-
-}
